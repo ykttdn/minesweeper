c64b14a548112831d627ec131f8aa4e9310dcc47
ykttdn <38461803+ykttdn@users.noreply.github.com>
2022-06-23 14:25:33 +0900

    Add function

--- main.js
+++ main.js
@@ -19,6 +19,23 @@ let safeCellCount = height*width-mines;
 let remainingMines = mines;
 
 const remains = document.getElementsByClassName('remains')[0];
+const setMineCounter = remainingMines => {
+  if (remainingMines <= -100) {
+    remains.textContent = `-99`;
+  } else if (remainingMines <= -10) {
+    remains.textContent = remainingMines;
+  } else if (remainingMines <= -1) {
+    remains.textContent = ` ${remainingMines}`;
+  } else if (remainingMines <= 9) {
+    remains.textContent = `00${remainingMines}`;
+  } else if (remainingMines <= 99) {
+    remains.textContent = `0${remainingMines}`;
+  } else if (remainingMines <= 999) {
+    remains.textContent = remainingMines;
+  } else {
+    remains.textContent = `999`;
+  }
+}
 if (remainingMines < 10) {
   remains.textContent = `00${remainingMines}`;
 } else if (remainingMines < 100) {
