811b8f0bf2f22452e56d2a3957bd875be3435a05
ykttdn <38461803+ykttdn@users.noreply.github.com>
2022-08-18 00:04:22 +0900

    Enable level selection

--- minesweeper/src/App.vue
+++ minesweeper/src/App.vue
@@ -1,14 +1,38 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import MainHeader from "./components/MainHeader.vue";
 import LevelSelector from "./components/LevelSelector.vue";
 import PlaySection from "./components/PlaySection.vue";
 import BottomSection from "./components/BottomSection.vue";
+
+const rowSize = ref(9);
+const columnSize = ref(9);
+const mineNumber = ref(10);
+
+const changeParameters = (selectedLevel: string) => {
+  if (selectedLevel === "normal") {
+    rowSize.value = 16;
+    columnSize.value = 16;
+    mineNumber.value = 40;
+  } else if (selectedLevel === "hard") {
+    rowSize.value = 16;
+    columnSize.value = 30;
+    mineNumber.value = 99;
+  } else {
+    rowSize.value = 9;
+    columnSize.value = 9;
+    mineNumber.value = 10;
+  }
+};
 </script>
 
 <template>
   <MainHeader></MainHeader>
   <main class="wrapper">
-    <LevelSelector></LevelSelector>
+    <LevelSelector @change-parameters="changeParameters"></LevelSelector>
+    <span>{{ " " + rowSize }}</span>
+    <span>{{ " " + columnSize }}</span>
+    <span>{{ " " + mineNumber }}</span>
     <PlaySection></PlaySection>
     <BottomSection></BottomSection>
   </main>
--- minesweeper/src/components/LevelSelector.vue
+++ minesweeper/src/components/LevelSelector.vue
@@ -1,10 +1,25 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const level = ref("easy");
+const emit = defineEmits(["changeParameters"]);
+const changeLevel = (selectedLevel: string) =>
+  emit("changeParameters", selectedLevel);
+</script>
+
 <template>
   <div class="level-selection">
     <label for="level">Level:</label>
-    <select name="level" id="level">
+    <select
+      name="level"
+      id="level"
+      v-model="level"
+      @change="changeLevel(level)"
+    >
       <option value="easy">EASY</option>
       <option value="normal">NORMAL</option>
       <option value="hard">HARD</option>
     </select>
   </div>
+  <span>{{ level }}</span>
 </template>
