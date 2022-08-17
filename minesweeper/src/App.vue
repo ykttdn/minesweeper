65c008f524f5bbfd22f5a4d259d994ec78017abc
ykttdn <38461803+ykttdn@users.noreply.github.com>
2022-08-17 22:49:04 +0900

    Add level selector

--- minesweeper/src/App.vue
+++ minesweeper/src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import MainHeader from "./components/MainHeader.vue";
+import LevelSelector from "./components/LevelSelector.vue";
 import PlaySection from "./components/PlaySection.vue";
 </script>
 
 <template>
   <MainHeader></MainHeader>
   <main class="wrapper">
+    <LevelSelector></LevelSelector>
     <PlaySection></PlaySection>
   </main>
 </template>
---
+++ minesweeper/src/components/LevelSelector.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="level-selection">
+    <label for="level">Level:</label>
+    <select name="level" id="level">
+      <option value="easy">EASY</option>
+      <option value="normal">NORMAL</option>
+      <option value="hard">HARD</option>
+    </select>
+  </div>
+</template>
