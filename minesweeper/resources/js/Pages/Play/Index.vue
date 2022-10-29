1352c75d3dc045503392f7fe461e4ee39c701965
ykttdn <38461803+ykttdn@users.noreply.github.com>
2022-10-29 17:23:36 +0900

    #81 Add prototype of minesweeper

--- minesweeper/resources/js/Pages/Play/Index.vue
+++ minesweeper/resources/js/Pages/Play/Index.vue
@@ -14,9 +14,44 @@ const result = useForm({
 </script>
 
 <template>
-    <Head title="Minesweeper" />
+    <Head>
+        <title>Minesweeper</title>
+
+        <!-- reset CSS -->
+        <link
+            rel="stylesheet"
+            href="https://unpkg.com/ress/dist/ress.min.css"
+        />
+
+        <!-- Google Fonts - Roboto Mono -->
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Philosopher:wght@700&family=Roboto+Mono:wght@700&display=swap"
+            rel="stylesheet"
+        />
+    </Head>
 
     <AuthenticatedLayout>
+        <div class="wrapper">
+            <div class="play-area">
+                <div class="top-area">
+                    <div class="mine-counter">000</div>
+                    <button class="reset-button"></button>
+                    <div class="timer">000</div>
+                </div>
+                <div class="board">
+                    <div v-for="row in 9" :key="row" class="row">
+                        <div
+                            v-for="column in 9"
+                            :key="column"
+                            class="cell cell--unopened"
+                        ></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
             <form
                 @submit.prevent="
