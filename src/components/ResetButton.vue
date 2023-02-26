5353dd4d0c1796379df0798812ff8ea7a39d99de
ykttdn <38461803+ykttdn@users.noreply.github.com>
2023-02-26 15:19:49 +0900

    #82 Fix bug on reset button

--- src/components/ResetButton.vue
+++ src/components/ResetButton.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { useCellStore } from "@/stores/cell";
 import { useParametersStore } from "@/stores/parameters";
+import { storeToRefs } from "pinia";
 
 const parameters = useParametersStore();
-const { columnSize, initializeParameters, rowSize } = parameters;
+const { columnSize, rowSize } = storeToRefs(parameters);
+const { initializeParameters } = parameters;
 
 const { initializeCells } = useCellStore();
 </script>
