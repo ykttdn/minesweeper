c9bd9566386bfa26f0507f9a1214065eee9b97b7
ykttdn <38461803+ykttdn@users.noreply.github.com>
2023-02-27 12:38:13 +0900

    #82 Update reset button to change face

--- src/components/ResetButton.vue
+++ src/components/ResetButton.vue
@@ -1,18 +1,35 @@
 <script setup lang="ts">
 import { useCellStore } from "@/stores/cell";
 import { useParametersStore } from "@/stores/parameters";
+import {
+  FACE_FAILURE,
+  FACE_NORMAL,
+  FACE_SUCCESS,
+} from "@/utils/GameParameters";
 import { storeToRefs } from "pinia";
+import { computed } from "vue";
 
 const parameters = useParametersStore();
-const { columnSize, rowSize } = storeToRefs(parameters);
+const { columnSize, hasOpenedAllSafeCells, hasOpenedMinedCell, rowSize } =
+  storeToRefs(parameters);
 const { initializeParameters } = parameters;
 
 const { initializeCells } = useCellStore();
+
+const buttonState = computed(() => {
+  if (hasOpenedAllSafeCells.value) {
+    return FACE_SUCCESS;
+  } else if (hasOpenedMinedCell.value) {
+    return FACE_FAILURE;
+  } else {
+    return FACE_NORMAL;
+  }
+});
 </script>
 
 <template>
   <button
-    class="reset-button face-normal"
+    :class="buttonState"
     @click="initializeCells(rowSize, columnSize), initializeParameters()"
   ></button>
 </template>
