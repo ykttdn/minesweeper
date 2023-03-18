39575fb6ec3fdc54abe1da934143f7f800802013
ykttdn <38461803+ykttdn@users.noreply.github.com>
2023-03-18 16:57:36 +0900

    Ignore Laravel project 'cos it's unnecessary in Netlify

--- minesweeper/resources/js/Pages/Play/Minesweeper/components/RemainsCounter.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { inject } from "vue";
-
-const displayedMineNumber = inject("displayedMineNumber");
-</script>
-
-<template>
-    <div class="mine-counter">{{ displayedMineNumber }}</div>
-</template>
