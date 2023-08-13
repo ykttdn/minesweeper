7e99879f3f0a9b288f68fc189682c302cf73249f
ykttdn <38461803+ykttdn@users.noreply.github.com>
2023-08-13 22:49:07 +0900

    #99 Add unit test

--- tests/unit/timerModules.test.ts
+++ tests/unit/timerModules.test.ts
@@ -1,7 +1,9 @@
 // @vitest-environment jsdom
 
 import { afterEach, beforeEach, describe, expect, test, vi } from "vitest";
+import { resetTimer } from "../../src/stores/timerModules/resetTimer";
 import { startTimer } from "../../src/stores/timerModules/startTimer";
+import { stopTimer } from "../../src/stores/timerModules/stopTimer";
 import type { Timer } from "../../src/types/timer";
 
 describe("Timer modules", () => {
@@ -13,7 +15,7 @@ describe("Timer modules", () => {
     vi.restoreAllMocks();
   });
 
-  test("should start timer", () => {
+  test("should start & stop timer", () => {
     const timer: Timer = startTimer({ id: 0, current: 0 });
 
     expect(timer.id).not.toBe(0);
@@ -29,5 +31,23 @@ describe("Timer modules", () => {
 
     vi.advanceTimersByTime(1);
     expect(timer.current).toBe(2);
+
+    vi.advanceTimersByTime(1000);
+    const current: number = timer.current;
+    stopTimer(timer);
+    vi.advanceTimersByTime(1000);
+    expect(timer.current).toBe(current);
+  });
+
+  test("should reset timer", () => {
+    const timer: Timer = startTimer({ id: 0, current: 0 });
+
+    vi.advanceTimersByTime(1000);
+
+    const newTimer: Timer = resetTimer(timer);
+
+    vi.advanceTimersByTime(5000);
+
+    expect(newTimer).toStrictEqual({ id: 0, current: 0 });
   });
 });
