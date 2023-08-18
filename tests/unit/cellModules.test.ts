bd8b98bfab5fde40025da3685df3cf480933a14f
ykttdn <38461803+ykttdn@users.noreply.github.com>
2023-08-18 20:31:45 +0900

    #99 Add unit test

--- tests/unit/cellModules.test.ts
+++ tests/unit/cellModules.test.ts
@@ -8,6 +8,7 @@ import {
   COLUMN_SIZE_HARD,
 } from "../../src/utils/GameParameters";
 import { random } from "../../src/utils/random";
+import { getAdjacentCellsIndex } from "../../src/utils/GetAdjacentCellsIndex";
 import { setBoardParams } from "../../src/stores/paramsModules/setBoardParams";
 
 describe("Cell modules", () => {
@@ -63,5 +64,94 @@ describe("Cell modules", () => {
       });
     });
     expect(count).toBe(boardParams.mineNumber);
+  });
+
+  test("should return adjacent cells' indices", () => {
+    const boardParams: BoardParams = setBoardParams("easy");
+
+    expect(
+      getAdjacentCellsIndex(0, 0, boardParams.rowSize, boardParams.columnSize)
+    ).toStrictEqual([
+      [0, 1],
+      [1, 0],
+      [1, 1],
+    ]);
+
+    expect(
+      getAdjacentCellsIndex(0, 4, boardParams.rowSize, boardParams.columnSize)
+    ).toStrictEqual([
+      [0, 3],
+      [0, 5],
+      [1, 3],
+      [1, 4],
+      [1, 5],
+    ]);
+
+    expect(
+      getAdjacentCellsIndex(0, 8, boardParams.rowSize, boardParams.columnSize)
+    ).toStrictEqual([
+      [0, 7],
+      [1, 7],
+      [1, 8],
+    ]);
+
+    expect(
+      getAdjacentCellsIndex(4, 0, boardParams.rowSize, boardParams.columnSize)
+    ).toStrictEqual([
+      [3, 0],
+      [3, 1],
+      [4, 1],
+      [5, 0],
+      [5, 1],
+    ]);
+
+    expect(
+      getAdjacentCellsIndex(4, 4, boardParams.rowSize, boardParams.columnSize)
+    ).toStrictEqual([
+      [3, 3],
+      [3, 4],
+      [3, 5],
+      [4, 3],
+      [4, 5],
+      [5, 3],
+      [5, 4],
+      [5, 5],
+    ]);
+
+    expect(
+      getAdjacentCellsIndex(4, 8, boardParams.rowSize, boardParams.columnSize)
+    ).toStrictEqual([
+      [3, 7],
+      [3, 8],
+      [4, 7],
+      [5, 7],
+      [5, 8],
+    ]);
+
+    expect(
+      getAdjacentCellsIndex(8, 0, boardParams.rowSize, boardParams.columnSize)
+    ).toStrictEqual([
+      [7, 0],
+      [7, 1],
+      [8, 1],
+    ]);
+
+    expect(
+      getAdjacentCellsIndex(8, 4, boardParams.rowSize, boardParams.columnSize)
+    ).toStrictEqual([
+      [7, 3],
+      [7, 4],
+      [7, 5],
+      [8, 3],
+      [8, 5],
+    ]);
+
+    expect(
+      getAdjacentCellsIndex(8, 8, boardParams.rowSize, boardParams.columnSize)
+    ).toStrictEqual([
+      [7, 7],
+      [7, 8],
+      [8, 7],
+    ]);
   });
 });
