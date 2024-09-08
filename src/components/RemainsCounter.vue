3d84391b6354219081cc3df96e5cbedc4b208805
ykttdn <38461803+ykttdn@users.noreply.github.com>
2024-09-08 15:56:11 +0900

    Replace component

--- src/components/RemainsCounter.vue
+++ src/components/RemainsCounter.vue
@@ -1,32 +1,12 @@
 <script setup lang="ts">
 import { useParametersStore } from "@/stores/parameters";
 import { storeToRefs } from "pinia";
-import { computed } from "vue";
+import DigitalDisplay from "./DigitalDisplay.vue";
 
-const parameters = useParametersStore();
-const { gameParams } = storeToRefs(parameters);
-
-const displayedMineNumber = computed(() => {
-  const { remainingMineNumber } = gameParams.value;
-
-  if (remainingMineNumber <= -100) {
-    return "-99";
-  } else if (remainingMineNumber <= -10) {
-    return `${remainingMineNumber}`;
-  } else if (remainingMineNumber <= -1) {
-    return `- ${-remainingMineNumber}`;
-  } else if (remainingMineNumber <= 9) {
-    return `00${remainingMineNumber}`;
-  } else if (remainingMineNumber <= 99) {
-    return `0${remainingMineNumber}`;
-  } else if (remainingMineNumber <= 999) {
-    return `${remainingMineNumber}`;
-  } else {
-    return "999";
-  }
-});
+const parametersStore = useParametersStore();
+const { gameParams } = storeToRefs(parametersStore);
 </script>
 
 <template>
-  <div class="remains">{{ displayedMineNumber }}</div>
+  <DigitalDisplay :number="gameParams.remainingMineNumber"></DigitalDisplay>
 </template>
